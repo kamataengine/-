abaa249276d28667a85b3d98b51936ab057851cd
saaya <k022g1024@g.neec.ac.jp>
2022-11-25 09:33:51 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,59 @@
+﻿#include <Novice.h>
+
+const char kWindowTitle[] = "GC1B_09_オオオカサアヤ";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	int posX = 50;
+	int speed = 5;
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+		if (posX >= 1230)
+		{
+			posX = 1280 - 50;
+		}
+		
+			
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+		Novice::DrawEllipse(posX, 100, 50, 50, 0.0f, RED, kFillModeSolid);
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
